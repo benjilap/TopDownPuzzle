fda545a96a31af579318c2b9f21eda4874f68416
benjilap <benjilap2@yahoo.fr>
2019-03-12 15:59:55 +0100

    up jump

--- BG_PuzzleGame/Assets/Benji/Scripts/PlayerControls.cs
+++ BG_PuzzleGame/Assets/Benji/Scripts/PlayerControls.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour {
 
-    public float playerSpeed=1;
+    [SerializeField]
+    float jumpForce=1;
+    [SerializeField]
+    float playerSpeed=1;
+
+    bool canJump;
+
+    //CheckJumpTimer
+    bool startCheckJump;
+    float saveTimeCheck;
+    float timerCheck;
+    float timeToReset = 0.4f;
 
     Vector3 xAxis;
     Vector3 yAxis;
@@ -19,7 +30,8 @@ public class PlayerControls : MonoBehaviour {
 	void Update () {
         UpdatePlayerAxis();
         PlayerMovement();
-
+        PlayerJump();
+        
 	}
 
     void UpdatePlayerAxis()
@@ -37,10 +49,44 @@ public class PlayerControls : MonoBehaviour {
         Vector3 gravity = new Vector3(0, this.GetComponent<Rigidbody>().velocity.y, 0);
         if(Input.GetAxis("Horizontal")!=0|| Input.GetAxis("Vertical") != 0)
         {
-            this.GetComponent<Rigidbody>().velocity = gravity+((xAxis * Input.GetAxis("Horizontal"))+ (yAxis * Input.GetAxis("Vertical")))*playerSpeed;
+            this.GetComponent<Rigidbody>().velocity = gravity+(((xAxis * Input.GetAxis("Horizontal"))+ (yAxis * Input.GetAxis("Vertical")))).normalized*playerSpeed;
 
         }
 
+    }
+
+    void PlayerJump()
+    {
+        if (this.GetComponent<Rigidbody>().velocity.y > -2 && this.GetComponent<Rigidbody>().velocity.y < 2)
+        {
+            if (Input.GetButtonDown("Jump") && canJump)
+            {
+
+                canJump = false;
+                this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 500, 0) * jumpForce);
+
+            }
+            else if (!canJump)
+            {
+
+                if (!startCheckJump)
+                {
+                    startCheckJump = true;
+                    saveTimeCheck = Time.time;
+                }
+                timerCheck = Time.time - saveTimeCheck;
+                if (timerCheck >= timeToReset)
+                {
+                    startCheckJump = false;
+                    canJump = true;
+                }
+            }
+        }
+        else
+        {
+            startCheckJump = false;
+
+        }
     }
 
 
