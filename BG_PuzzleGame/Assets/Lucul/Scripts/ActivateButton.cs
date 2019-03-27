f8435bda455a9459ba8183c90e4e1f3a20067005
Lucas Lecante <38083658+Lucul@users.noreply.github.com>
2019-03-27 16:39:36 +0100

    up

---
+++ BG_PuzzleGame/Assets/Lucul/Scripts/ActivateButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateButton : MonoBehaviour {
+
+
+
+
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<Animator>().SetInteger("ButtonState", 1);
+        }
+    }
+}
