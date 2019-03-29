1f0fa2255e21e9a58c58ff2ec0805481bd5bb0d1
Lucas Lecante <38083658+Lucul@users.noreply.github.com>
2019-03-29 12:17:38 +0100

    tuto

---
+++ BG_PuzzleGame/Assets/Lucul/Scripts/StairActivateButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairActivateButton : MonoBehaviour
+{
+
+
+
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            transform.GetChild(0).GetChild(0).GetComponent<Animator>().SetInteger("ButtonState", 1);
+            transform.parent.GetComponent<UnlockStairs>().StairsState = 1;
+        }
+    }
+}
---
+++ BG_PuzzleGame/Assets/Lucul/Scripts/UnlockStairs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class UnlockStairs : MonoBehaviour {
+
+    public int StairsState;
+    
+	
+	
+	void Update () {
+        transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<Animator>().SetInteger("Door_State", DoorState);
+    }
+}
