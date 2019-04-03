000ba97ebde7754a4323c462c794e076fa542b44
benjilap <benjilap2@yahoo.fr>
2019-04-03 16:08:04 +0200

    up spell

--- BG_PuzzleGame/Assets/Benji/Scripts/GameScript/PlayerControls.cs
+++ BG_PuzzleGame/Assets/Benji/Scripts/GameScript/PlayerControls.cs
@@ -34,24 +34,29 @@ public class PlayerControls : MonoBehaviour {
 
     public float yVelocity;
 
+    int useSpellState;
+    GameObject spellSpawner;
+    GameObject spellPrefab;
+    GameObject spellCasted;
+    
     void Start () {
         myPivotCamera = GameObject.FindObjectOfType<CameraControls>().gameObject;
         charAtor = this.transform.GetChild(1).GetComponent<Animator>();
         charPos = this.transform.GetChild(1).localPosition;
         charAngle = this.transform.GetChild(1).localRotation;
         charScale = this.transform.GetChild(1).localScale;
-
+        spellSpawner = charAtor.transform.Find("Root/Hips/Spine_01/Spine_02/Spine_03/Clavicle_R/Shoulder_R/Elbow_R/Hand_R/SpellPoint").gameObject;
+        spellPrefab = Resources.Load<GameObject>("Player/SpellPrefab");
     }
 	
 	void FixedUpdate () {
         UpdatePlayerAxis();
 
+        CastSpell();
         PlayerMovement();
         PlayerJump();
         MoveAnimControl();
 
-        yVelocity = this.GetComponent<Rigidbody>().velocity.y;
-        Debug.Log(yVelocity);
 
     }
 
@@ -66,15 +71,16 @@ public class PlayerControls : MonoBehaviour {
 
     void PlayerMovement()
     {
+
+        playerDir = ((xAxis * Input.GetAxis("Horizontal")) + (yAxis * Input.GetAxis("Vertical")));
+        playerDirNorm = playerDir.normalized;
+        Vector3 gravity = new Vector3(0, this.GetComponent<Rigidbody>().velocity.y, 0);
+        RaycastHit hit;
+        Debug.DrawRay(this.transform.position + new Vector3(0, -0.4f, 0), playerDirNorm * 0.4f);
+        //Debug.DrawRay(this.transform.position + new Vector3(0, -0.4f, 0), UpdateVecWallDetect(tempPlayerDir, 40));
+        //Debug.DrawRay(this.transform.position + new Vector3(0, -0.4f, 0), UpdateVecWallDetect(tempPlayerDir, -40));
         if (canMove)
         {
-            playerDir = ((xAxis * Input.GetAxis("Horizontal")) + (yAxis * Input.GetAxis("Vertical")));
-            playerDirNorm = playerDir.normalized;
-            Vector3 gravity = new Vector3(0, this.GetComponent<Rigidbody>().velocity.y, 0);
-            RaycastHit hit;
-            Debug.DrawRay(this.transform.position + new Vector3(0, -0.4f, 0), playerDirNorm * 0.4f);
-            //Debug.DrawRay(this.transform.position + new Vector3(0, -0.4f, 0), UpdateVecWallDetect(tempPlayerDir, 40));
-            //Debug.DrawRay(this.transform.position + new Vector3(0, -0.4f, 0), UpdateVecWallDetect(tempPlayerDir, -40));
             if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
             {
 
@@ -102,9 +108,9 @@ public class PlayerControls : MonoBehaviour {
             }
             else if (Input.GetAxis("Horizontal") == 0 & Input.GetAxis("Vertical") == 0)
             {
-                if (this.GetComponent<Rigidbody>().velocity.y < 0 )
+                if (this.GetComponent<Rigidbody>().velocity.y < 0)
                 {
-                    if (canJump&&!hasJump)
+                    if (canJump && !hasJump)
                     {
                         this.GetComponent<Rigidbody>().velocity = Vector3.zero;
 
@@ -114,23 +120,25 @@ public class PlayerControls : MonoBehaviour {
         }
     }
 
+
     void PlayerJump()
     {
-        Debug.DrawRay(this.transform.position, Vector3.down*0.9f+ playerDirNorm*0.05f, Color.green);
+
+        Debug.DrawRay(this.transform.position, Vector3.down * 0.9f + playerDirNorm * 0.05f, Color.green);
 
         RaycastHit jumpDetect;
         RaycastHit wallDetect;
-        if (Physics.Raycast(this.transform.position, Vector3.down + playerDirNorm*0.05f, out jumpDetect,0.9f))
+        if (Physics.Raycast(this.transform.position, Vector3.down + playerDirNorm * 0.05f, out jumpDetect, 0.9f))
         {
             canJump = true;
             hasJump = false;
 
             if (!Physics.Raycast(this.transform.position + new Vector3(0, -0.4f, 0), playerDirNorm, out wallDetect, tempPlayerDir.magnitude * 0.4f) & !Physics.Raycast(this.transform.position + new Vector3(0, 0.4f, 0), playerDirNorm, out wallDetect, tempPlayerDir.magnitude * 0.5f))
             {
-                
+
                 if (this.GetComponent<Rigidbody>().velocity.y > -0.6f && this.GetComponent<Rigidbody>().velocity.y < 0.6f)
                 {
-                    if (Input.GetButtonDown("Jump"))
+                    if (Input.GetButtonDown("Jump") && useSpellState==0)
                     {
                         hasJump = true;
                         this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 500, 0) * jumpForce);
@@ -145,25 +153,81 @@ public class PlayerControls : MonoBehaviour {
         {
             canJump = false;
         }
+    }
 
+
+    void CastSpell()
+    {
+        Debug.Log(Input.GetAxis("FireSpell"));
+        if (Input.GetAxis("FireSpell")>0)
+        {
+            if (useSpellState == 0)
+            {
+                useSpellState = 1;
+                canMove = false;
+                this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
+            else if (useSpellState == 1)
+            {
+                useSpellState = 2;
+                StartCoroutine(WaitTimeToModifSpellState(1,2));
+
+                
+
+            }
+        }
+        else if (useSpellState != 0)
+        {
+            if (spellCasted != null)
+            {
+                spellCasted.GetComponent<Rigidbody>().AddForce(tempPlayerDir);
+                spellCasted.GetComponent<Rigidbody>().useGravity = true;
+                spellCasted.transform.parent = null;
+                spellCasted = null;
+            }
+            float timeToReset = useSpellState;
+            useSpellState = 0;
+            if (!canMove)
+            {
+
+                StartCoroutine(WaitTimeToMove(timeToReset-0.1f*timeToReset));
+
+            }
+        }
+        
     }
 
     void MoveAnimControl()
     {
         //MoveAnim
-        charAtor.SetFloat("MovmentVar", playerDir.magnitude);
+        if (canMove)
+        {
+            charAtor.SetFloat("MovmentVar", playerDir.magnitude);
+        }
         charAtor.gameObject.transform.localPosition = charPos;
         charAtor.gameObject.transform.localRotation = charAngle;
         charAtor.gameObject.transform.localScale = charScale;
         
-
         //JumpAnim
         charAtor.SetBool("HasJump", hasJump);
         charAtor.SetBool("IsFalling", canJump);
 
         //RotationControl
-        this.transform.rotation = Quaternion.LookRotation(tempPlayerDir);
+        if (playerDirNorm.magnitude != 0)
+        {
+            this.transform.rotation = Quaternion.LookRotation(playerDirNorm);
+            tempPlayerDir = playerDirNorm;
+        }
+        else
+        {
+            this.transform.rotation = Quaternion.LookRotation(tempPlayerDir);
+        }
         this.transform.rotation = Quaternion.Euler(0, this.transform.eulerAngles.y, 0);
+
+        //SPellCastAnim
+        
+        charAtor.SetInteger("UseSpellState", useSpellState);
+        
 
     }
 
@@ -177,6 +241,26 @@ public class PlayerControls : MonoBehaviour {
 
             }
         }
+    }
+
+    IEnumerator WaitTime(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        print("WaitAndPrint " + Time.time);
+
+    }
+    IEnumerator WaitTimeToModifSpellState(float timeToWait,int newSpellState)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        useSpellState = newSpellState;
+        spellCasted = Instantiate(spellPrefab, spellSpawner.transform.position, Quaternion.identity);
+        spellCasted.transform.SetParent(spellSpawner.transform);
+
+    }
+    IEnumerator WaitTimeToMove(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        canMove = true;
     }
 
     Vector3 ScalePlayerMove(Vector3 currentPlayerDir)
---
+++ BG_PuzzleGame/Assets/Benji/Scripts/GameScript/SpellInteract.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellInteract : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckCollision();
+	}
+
+    void CheckCollision()
+    {
+        RaycastHit ObstacleCollide;
+        if (Physics.Raycast(transform.position, this.GetComponent<Rigidbody>().velocity, out ObstacleCollide, 0.1f)){
+            if(ObstacleCollide.transform.gameObject.tag != "Player")
+            {
+            Destroy(this.gameObject);
+
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+    }
+}
