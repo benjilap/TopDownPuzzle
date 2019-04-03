4a4729822eef46d3dabb310b2158367620ed1e75
Lucas Lecante <38083658+Lucul@users.noreply.github.com>
2019-04-03 15:09:05 +0200

    up tuto

---
+++ BG_PuzzleGame/Assets/Lucul/Scripts/PlaySound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+
+    void DoorSound()
+    {
+        audioSource.PlayOneShot(audioClip);
+
+    }
+}
