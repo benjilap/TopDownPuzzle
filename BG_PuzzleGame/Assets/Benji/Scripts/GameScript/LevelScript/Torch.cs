eb9c0a86e9e31edbb8944f5f43ee175c3834a8c6
benjilap <benjilap2@yahoo.fr>
2019-05-01 17:47:38 +0200

    torchesSystem

---
+++ BG_PuzzleGame/Assets/Benji/Scripts/GameScript/LevelScript/Torch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour {
+
+    [SerializeField]
+    float enlightedTime;
+    [HideInInspector]
+    public bool lighted;
+    bool beEnlighted;
+    float saveTime;
+
+    private void Update()
+    {
+        if (enlightedTime != 0)
+        {
+            EnlightedTime();
+        }
+    }
+
+    void EnlightedTime()
+    {
+        if (lighted)
+        {
+            if (!beEnlighted)
+            {
+                saveTime = Time.time;
+                beEnlighted = true;
+            }
+            if (Time.time >= saveTime + enlightedTime)
+            {
+                beEnlighted = false;
+                lighted = false;
+            }
+        }
+    }
+}
---
+++ BG_PuzzleGame/Assets/Benji/Scripts/GameScript/LevelScript/TorchsSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchsSystem : MonoBehaviour {
+
+    [HideInInspector]
+    public bool lightedTorches;
+
+    List<Torch> Torches = new List<Torch>();
+
+	void Start () {
+        SetTorchesSys();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckTorches();
+	}
+
+    void SetTorchesSys()
+    {
+        for(int i=0; i<this.transform.childCount; i++)
+        {
+            if (this.transform.GetChild(i).GetComponent<Torch>() != null)
+            {
+                Torches.Add(this.transform.GetChild(i).GetComponent<Torch>());
+            }
+        }
+    }
+
+    void CheckTorches()
+    {
+        int lightedCheck = 0;
+        for (int i = 0; i < Torches.Count; i++)
+        {
+            if (Torches[i].lighted)
+            {
+                lightedCheck +=1;
+            }
+        }
+
+        if (lightedCheck == Torches.Count)
+        {
+            lightedTorches = true;
+        }
+    }
+}
